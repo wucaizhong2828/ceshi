aa6f2da5032793774e79adc3537a4544bd8aa428
wucaizhong2828 <156386476+wucaizhong2828@users.noreply.github.com>
2025-07-17 12:45:33 +0800

    Delete 海纳.js

--- 海纳.js
+++
@@ -1,5 +0,0 @@
-{
-        "主页url": "https://hainatv.net/",
-        "分类url": "https://hainatv.net/index.php/vod/show/area/{area}/by/{by}/id/{cateId}/lang/{lang}/letter/{letter}/page/{catePg}/year/{year}.html",
-        "分类": "电影$1#剧集$2#综艺$3#动漫$4#短剧$59#解说$64"
-      }
